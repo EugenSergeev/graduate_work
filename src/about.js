292933ad52cf350ca5a0335793de61deb2b422f6
Evgeny Sergeev <flashser@yandex.ru>
2019-11-23 23:48:28 +0300

    slider complete

--- src/about.js
+++ src/about.js
@@ -1,16 +1,11 @@
 import "./pages/about.css"
-import "../node_modules/@glidejs/glide/dist/css/glide.core.min.css"
-import "../node_modules/@glidejs/glide/dist/glide.min.js"
+import "./blocks/swiper/swiper.css"
+import "./vendor/swiper.js"
 
-import Glide from '@glidejs/glide'
-import { Controls, Breakpoints } from '../node_modules/@glidejs/glide/dist/glide.modular.esm'
-
-var glide = new Glide('.glide', {
-  type: 'slide',
-  perView: 4,
-  gap: 16,
-  peek: 1,
-  focusAt: 'center',
-})
-
-glide.mount({ Controls, Breakpoints })
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 100,
+  effect: "slide",
+  spaceBetween: 16,
+  //slidesPerView: auto,
+  centeredSlides: true
+});

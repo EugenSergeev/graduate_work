9b66436ce2f3bade3618e354ad1ccbf02dc3579d
EugenSergeev <flashser@yandex.ru>
2020-02-14 19:49:21 +0300

    Dates: const -> let

--- src/js/dates.js
+++ src/js/dates.js
@@ -23,9 +23,9 @@ class Dates {
     }
   }
   newsApiFormat = (date) => {
-    const dd = date.getDate();
-    const mm = date.getMonth() + 1;
-    const yyyy = date.getFullYear();
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1;
+    let yyyy = date.getFullYear();
 
     if (dd < 10) dd = '0' + dd;
     if (mm < 10) mm = '0' + mm;
